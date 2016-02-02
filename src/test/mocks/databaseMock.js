ab738f69c1ce65a8d78253e90361ad7cf13e21fc
Chris Perks <pop.chris@gmail.com>
2016-02-02 21:24:27 +0000

    Expanded functionality of databaseMock.js

--- src/test/mocks/databaseMock.js
+++ src/test/mocks/databaseMock.js
@@ -1,27 +1,71 @@
 import Sequelize from 'sequelize';
 import _ from 'lodash';
 import Config from '../../config/config';
-import Properties from '../../database/models/properties';
-import PropertyTypes from '../../database/models/propertyTypes';
-import Nodes from '../../database/models/nodes';
-import ChildNodes from '../../database/models/childNodes';
+import Properties from '../../database/models/property';
+import PropertyTypes from '../../database/models/propertyType';
+import Nodes from '../../database/models/node';
+import ChildNodes from '../../database/models/childNode';
 import Constants from '../../constants/constants'
 
 const databaseMock = {
     models : {
         nodes : {
             findAll : function(){
-                return [1,2,3];
+                let data = [];
+                let i = 0;
+                _.times(5, () => {
+                    data.push({
+                        nodeId: ++i,
+                        text: 'Fruits of the world',
+                        newest: true,
+                        versionId: (12345 * i),
+                        getProperties: function(){
+                            return {
+                                then: function(){
+                                    return databaseMock.models.properties.findAll();
+                                }
+                            }
+                        },
+                        getChildNodes: function(){
+                            return {
+                                then: function(){
+                                    return databaseMock.models.nodes.findAll();
+                                }
+                            }
+                        }
+                    });
+                });
+                return data;
             }
         },
         properties: {
             findAll : function(){
-                return [1,2,3];
+
+                let data = [];
+                _.times(5, () => {
+
+                        data.push({
+                            value: function(){
+                              return {
+                                  dataNtext: 'Apple'
+                              }
+                            },
+                            key: 'Type of fruit',
+                            getPropertyType: function(){
+                                return {
+                                    then: function(){
+                                        return 'Picker';
+                                    }
+                                }
+                            }
+                    });
+                });
+                return data;
             }
         },
         propertyTypes: {
             findAll : function(){
-                return [1,2,3];
+                return [];
             }
         }
     }
