bdc227d756b81ce21b95a7285f144017c4b9243e
Chris Perks <pop.chris@gmail.com>
2016-02-02 21:23:06 +0000

    Updated references for db module

--- src/database/db.js
+++ src/database/db.js
@@ -1,10 +1,10 @@
 import Sequelize from 'sequelize';
 import _ from 'lodash';
 import Config from './config/config';
-import Properties from 'models/properties';
-import PropertyTypes from 'models/propertyTypes';
-import Nodes from 'models/nodes';
-import ChildNodes from 'models/childNodes';
+import Properties from 'models/property';
+import PropertyTypes from 'models/propertyType';
+import Nodes from 'models/node';
+import ChildNodes from 'models/childNode';
 import Constants from '../constants/constants'
 
 const Conn = new Sequelize(
@@ -28,7 +28,6 @@ const Property = Conn.define('properties', Properties,
         tableName: Constants.DB_TABLE_PROPERTYDATA
     }
 );
-
 
 const PropertyType = Conn.define('propertyTypes', PropertyTypes,
     {
