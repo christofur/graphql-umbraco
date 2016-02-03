435a486b61d0eacb03d3e319e8f16d067ca0dceb
Chris Perks <pop.chris@gmail.com>
2016-02-03 14:09:51 +0000

    Fixed reference to Schema function

--- src/test/schemaTests.js
+++ src/test/schemaTests.js
@@ -1,9 +1,9 @@
 import { expect } from 'chai';
 import { should, describe, it } from 'mocha';
 import request from 'supertest-as-promised';
-import express from 'express';
+import Express from 'express';
 import GraphHTTP from 'express-graphql';
-import Schema from '../schema/schema';
+import { Schema } from '../schema/schema';
 import Config from '../config/config';
 import dbMock from './mocks/databaseMock';
 
