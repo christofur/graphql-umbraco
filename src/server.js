66e8651d9c7860d6179f4da2b605a5ae03414481
christofur <pop.chris@gmail.com>
2016-08-02 20:52:54 +0100

    Removed mocking reference

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import Express from 'express';
 import GraphHTTP from 'express-graphql';
 import { Schema } from './schema/schema';
 import Config from './config/config';
-//import Db from './database/db';
-import Db from './test/mocks/databaseMock';
+import Db from './database/db';
+//import Db from './test/mocks/databaseMock';
 
 const APP_PORT = Config.ServerConfig.appPort;
 
